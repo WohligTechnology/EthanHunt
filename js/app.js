9da441d15eab2ea0a78817c0f9f46a309f666dc2
Chintan Shah <chintan@wohlig.com>
2015-08-01 20:59:16 +0530

    changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 // JavaScript Document
 var firstapp = angular.module('firstapp', [
-    'ui.router',
     'phonecatControllers',
     'templateservicemod',
     'navigationservice'
@@ -16,18 +15,6 @@ firstapp.config(function($stateProvider, $urlRouterProvider, cfpLoadingBarProvid
         url: "/home",
         templateUrl: "views/template.html",
         controller: 'HomeCtrl'
-    })
-
-    .state('feature', {
-        url: "/feature",
-        templateUrl: "views/template.html",
-        controller: 'FeatureCtrl'
-    })
-    
-    .state('infinite', {
-        url: "/infinite",
-        templateUrl: "views/template.html",
-        controller: 'InfiniteCtrl'
     })
 
     $urlRouterProvider.otherwise("/home");
