f41c699ddd9eb4fc43a0ade24dde011b0a81cfd9
mahesh maurya <mahesh@wohlig.com>
2015-08-13 15:20:07 +0530

    team done

--- js/controllers.js
+++ js/controllers.js
@@ -1,62 +1,65 @@
 angular.module('wohligController', ['templateservicemod', 'navigationservice', 'valdr'])
 
-.controller('WohligCtrl', function ($scope, TemplateService, NavigationService) {
+.controller('WohligCtrl', function($scope, TemplateService, NavigationService) {
         $scope.template = TemplateService;
         $scope.template.init();
     })
-    .controller('HomeCtrl', function ($scope, TemplateService, NavigationService) {
+    .controller('HomeCtrl', function($scope, TemplateService, NavigationService) {
         $scope.template = TemplateService;
         $scope.template.init();
         $scope.template.contentClass = false;
         $scope.template.menuheader = "";
+        $scope.template.backurl = "img/social/back.png";
     })
-    .controller('EnquiryCtrl', function ($scope, TemplateService, NavigationService) {
+    .controller('EnquiryCtrl', function($scope, TemplateService, NavigationService) {
         $scope.template = TemplateService;
         $scope.template.init();
         $scope.template.menuheader = "Enquiry";
+        $scope.template.backurl = "img/social/back.png";
     })
-    .controller('ReachusCtrl', function ($scope, TemplateService, NavigationService) {
+    .controller('ReachusCtrl', function($scope, TemplateService, NavigationService) {
         $scope.template = TemplateService;
-
         $scope.template.init();
         $scope.template.contentClass = true;
         $scope.template.hideContent = true;
         $scope.template.menuheader = "Reach Us";
+        $scope.template.backurl = "img/social/back.png";
     })
-    .controller('AboutCtrl', function ($scope, TemplateService, NavigationService) {
+    .controller('AboutCtrl', function($scope, TemplateService, NavigationService) {
         $scope.template = TemplateService;
         $scope.template.init();
         $scope.template.contentClass = true;
-        $scope.template.hideContent = true;
-        $scope.template.menuheader = "About";
-
+        //        $scope.template.hideContent = true;
+//        $scope.template.menuheader = "About";
+        $scope.template.backurl = "img/social/back2.png";
     })
-    .controller('ClientsCtrl', function ($scope, TemplateService, NavigationService) {
+    .controller('ClientsCtrl', function($scope, TemplateService, NavigationService) {
         $scope.template = TemplateService;
         $scope.template.init();
         $scope.template.menuheader = "Clients";
-    
-    $scope.slides = [
-				'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
-				'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
-				'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
-				'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
-			];
+        $scope.template.backurl = "img/social/back.png";
+
+        $scope.slides = [
+            'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
+            'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
+            'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
+            'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
+        ];
 
     })
-    .controller('PortfolioCtrl', function ($scope, TemplateService, NavigationService) {
+    .controller('PortfolioCtrl', function($scope, TemplateService, NavigationService) {
         $scope.template = TemplateService;
         $scope.template.init();
         $scope.template.menuheader = "Portfolio";
-
+        $scope.template.backurl = "img/social/back.png";
     })
-    .controller('TeamCtrl', function ($scope, TemplateService, NavigationService) {
+    .controller('TeamCtrl', function($scope, TemplateService, NavigationService) {
         $scope.template = TemplateService;
         $scope.template.init();
-        $scope.template.menuheader = "Team";
-
+//        $scope.template.menuheader = "Team";
+        $scope.template.backurl = "img/social/back.png";
     })
-    .controller('HeaderCtrl', function ($scope, TemplateService) {
+    .controller('HeaderCtrl', function($scope, TemplateService) {
         $scope.template = TemplateService;
         $scope.template.init();
     });
