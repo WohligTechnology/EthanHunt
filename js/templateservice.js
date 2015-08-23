f55462f5e2b17bd2f924e2a4ef76714f581a1beb
Chintan Shah <chintan@wohlig.com>
2015-08-24 03:23:14 +0530

    seo

--- js/templateservice.js
+++ js/templateservice.js
@@ -1,8 +1,8 @@
 var templateservicemod = angular.module('templateservicemod', []);
 templateservicemod.service('TemplateService', function() {
     this.title = "Universe of Creativity";
-    this.meta = "Wohlig,Wohlig Technology,Creative Agency,Development Firm,NodeJS,MongoDB,Web Development,Android,iOS,iPhone,iPad,iWatch,Selig,Web Designing";
-    this.metadesc = "Software development firm who have carved a niche in founding in the field of Web & App development. Projects that we've developed mainly show our creativity and excellence, we've made ourselves adaptable to the latest technology and we've developed a name in integrating various services on one platform with simplicity.";
+    this.meta = "Wohlig,Wohlig Technology,Creative Agency,Development Firm,NodeJS,MongoDB,Web Development,Android,iOS,iPhone,iPad,iWatch,Selig,Web Designing,ionic";
+    this.metadesc = "Software development firm who have carved a niche in founding in the field of Web,  App development, Ionic Development as well as Native Development. Projects that we've developed mainly show our creativity and excellence, we've made ourselves adaptable to the latest technology and we've developed a name in integrating various services on one platform with simplicity. ";
 
     var d = new Date();
     this.year = d.getFullYear();
