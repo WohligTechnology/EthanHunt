026b2d0e40b81ddc1ed91f41509187f0fbf70247
Rajmshah <raj.mshah2015@gmail.com>
2016-03-17 13:57:27 +0530

    portfolio updated

--- js/controllers.js
+++ js/controllers.js
@@ -77,171 +77,250 @@ angular.module('wohligController', ['templateservicemod', 'navigationservice'])
         $scope.template.backurl = "img/social/back.png";
 
         $scope.items = [
+          {
+              "id": 0,
+              "picture": "img/portfolio/JPP1.png",
+              "type": "mobile",
+              "name": "Jaipur Pink Panthers"
+      }, {
+              "id": 1,
+              "picture": "img/portfolio/tarun.png",
+              "type": "mobile",
+              "name": "Tarun Mitra Mandal"
+      }, {
+              "id": 2,
+              "picture": "img/portfolio/noteshare.png",
+              "type": "mobile",
+              "name": "NoteShare"
+      }, {
+              "id": 3,
+              "picture": "img/portfolio/sfa.jpg",
+              "type": "website",
+              "name": "SFA(Sports For All)"
+      },
+          {
+              "id": 4,
+              "picture": "img/portfolio/td.jpg",
+              "type": "website",
+              "name": "Ting Digital"
+      },
+          {
+              "id": 5,
+              "picture": "img/portfolio/jpp.jpg",
+              "type": "website",
+              "name": "Jaipur Pink Panthers"
+      }, {
+              "id": 6,
+              "picture": "img/portfolio/simi.jpg",
+              "type": "website",
+              "name": "Simi Events"
+      },
+          {
+              "id": 7,
+              "picture": "img/portfolio/access.jpg",
+              "type": "website",
+              "name": "Access World"
+      }, {
+              "id": 8,
+              "picture": "img/portfolio/aura.jpg",
+              "type": "website",
+              "name": "Auraart"
+      }, {
+              "id": 9,
+              "picture": "img/portfolio/ngu.jpg",
+              "type": "website",
+              "name": "WillNeverGrowUp"
+      }, {
+              "id": 10,
+              "picture": "img/portfolio/HQ.jpg",
+              "type": "website",
+              "name": "Happiness Quotient"
+      }, {
+              "id": 11,
+              "picture": "img/portfolio/adya.jpg",
+              "type": "website",
+              "name": "Adya"
+      }, {
+              "id": 12,
+              "picture": "img/portfolio/euro.jpg",
+              "type": "website",
+              "name": "Europratik"
+      }, {
+              "id": 13,
+              "picture": "img/portfolio/fynx.jpg",
+              "type": "website",
+              "name": "My Fynx"
+      }, {
+              "id": 14,
+              "picture": "img/portfolio/selfcare.jpg",
+              "type": "website",
+              "name": "SelfCare"
+      },
             {
-                "id": 0,
+                "id": 15,
                 "picture": "img/portfolio/bappa.jpg",
                 "type": "mobile",
                 "name": "Times Bappa"
         }, {
-                "id": 1,
+                "id": 16,
                 "picture": "img/portfolio/bussiness.jpg",
                 "type": "mobile",
                 "name": "Bussiness App"
         }, {
-                "id": 2,
+                "id": 17,
                 "picture": "img/portfolio/selig.jpg",
                 "type": "mobile",
                 "name": "Selig"
         }, {
-                "id": 3,
+                "id": 18,
                 "picture": "img/portfolio/moview.jpg",
                 "type": "mobile",
                 "name": "Moviews"
         },
             {
-                "id": 4,
+                "id": 19,
                 "picture": "img/portfolio/12.jpg",
                 "type": "mobile",
                 "name": "Best deal Finance"
         }, {
-                "id": 5,
+                "id": 20,
                 "picture": "img/portfolio/sfa.jpg",
                 "type": "website",
                 "name": "Sfanow (Sports For All)"
         },
             {
-                "id": 6,
+                "id": 21,
                 "picture": "img/portfolio/powerforone.jpg",
                 "type": "website",
                 "name": "Power For One"
         }, {
-                "id": 7,
+                "id": 22,
                 "picture": "img/portfolio/7.jpg",
                 "type": "website",
                 "name": "200 notout"
         },
             {
-                "id": 8,
+                "id": 23,
                 "picture": "img/portfolio/bpft.jpg",
                 "type": "website",
                 "name": "Blenders Pride"
         }, {
-                "id": 9,
-                "picture": "img/portfolio/Bherpo.jpg",
+                "id": 24,
+                "picture": "img/portfolio/bherpo1.png",
                 "type": "mobile",
                 "name": "Bherpo"
         }, {
-                "id": 10,
+                "id": 25,
                 "picture": "img/portfolio/4.jpg",
                 "type": "mobile",
                 "name": "Superu game"
         }, {
-                "id": 11,
+                "id": 26,
                 "picture": "img/portfolio/One-Stop-Barter.jpg",
                 "type": "mobile",
                 "name": "One Stop Barter"
         }, {
-                "id": 12,
+                "id": 27,
                 "picture": "img/portfolio/5.jpg",
                 "type": "website",
                 "name": "Anima creative"
         }, {
-                "id": 13,
+                "id": 28,
                 "picture": "img/portfolio/6.jpg",
                 "type": "mobile",
                 "name": "Stake App"
         }, {
-                "id": 14,
+                "id": 29,
                 "picture": "img/portfolio/toykraft.jpg",
                 "type": "mobile",
                 "name": "Toy Kraft Puzzle"
         }, {
-                "id": 15,
+                "id": 30,
                 "picture": "img/portfolio/predicto.jpg",
                 "type": "mobile",
                 "name": "Predicto"
         }, {
-                "id": 16,
+                "id": 31,
                 "picture": "img/portfolio/8.jpg",
                 "type": "website",
                 "name": "Reviu"
         },  {
-                "id": 17,
+                "id": 32,
                 "picture": "img/portfolio/10.jpg",
                 "type": "website",
                 "name": "Lodha group"
         }, {
-                "id": 18,
+                "id": 33,
                 "picture": "img/portfolio/11.jpg",
                 "type": "website",
                 "name": "Reniscience"
         }, {
-                "id": 19,
+                "id": 34,
                 "picture": "img/portfolio/facade.jpg",
                 "type": "website",
                 "name": "Facade Testing"
         }, {
-                "id": 20,
+                "id": 35,
                 "picture": "img/portfolio/13.jpg",
                 "type": "website",
                 "name": "Ziba collection"
         }, {
-                "id": 21,
+                "id": 36,
                 "picture": "img/portfolio/14.jpg",
                 "type": "website",
                 "name": "XDC india"
         }, {
-                "id": 22,
+                "id": 37,
                 "picture": "img/portfolio/15.jpg",
                 "type": "website",
                 "name": "world 7 mediacraft"
         }, {
-                "id": 23,
+                "id": 38,
                 "picture": "img/portfolio/16.jpg",
                 "type": "Mobile",
                 "name": "91 street"
         }, {
-                "id": 24,
+                "id": 39,
                 "picture": "img/portfolio/17.jpg",
                 "type": "website",
                 "name": "LBW Consult"
         }, {
-                "id": 25,
+                "id": 40,
                 "picture": "img/portfolio/18.jpg",
                 "type": "website",
                 "name": "Furnicheer"
         }, {
-                "id": 26,
+                "id": 41,
                 "picture": "img/portfolio/19.jpg",
                 "type": "mobile",
                 "name": "Mypacco App"
         }, {
-                "id": 27,
+                "id": 42,
                 "picture": "img/portfolio/orangestone.jpg",
                 "type": "website",
                 "name": "Orangestone"
         },{
-                "id": 28,
+                "id": 43,
                 "picture": "img/portfolio/20.jpg",
                 "type": "website",
                 "name": "Designers group"
         }, {
-                "id": 29,
+                "id": 44,
                 "picture": "img/portfolio/1.jpg",
                 "type": "website",
                 "name": "Lyla loves"
         }, {
-                "id": 30,
+                "id": 45,
                 "picture": "img/portfolio/9.jpg",
                 "type": "mobile",
                 "name": "Lyla loves App"
         }, {
-                "id": 31,
+                "id": 46,
                 "picture": "img/portfolio/2.jpg",
                 "type": "mobile",
                 "name": "Magic mirror App"
         }, {
-                "id": 32,
+                "id": 47,
                 "picture": "img/portfolio/3.jpg",
                 "type": "website",
                 "name": "Magic mirror"
